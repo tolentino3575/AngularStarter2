d4164a0a3d4a1d531c728ca982006ce8b4f60e31
Erik Tolentino <tolentino3575@gmail.com>
2018-01-18 14:37:16 -0800

    Add simple properties, functions, string interpolation

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello World</h1>`
+    template: `
+        <h1>Hello {{ name }} is {{ age }} years old</h1>
+        <h2>My name is {{ person.firstName }}. Last name {{ person.lastName }}</h2>    
+        <ul>
+            <li>{{ 'Hello World' }}</li>
+            <li>{{ 1+1 }}</li>
+            <li>{{ showAge() }}</li>
+        </ul>
+    `
 })
 
 export class SandboxComponent{
+    name = 'John Poe';
+    age = 45;
+    person = {
+        firstName: 'Steve',
+        lastName: 'Smith'
+    }
 
+    constructor(){
+        console.log('constructor running');
+        //this.age = 45;
+        this.hasBirthday();
+    }
+
+    hasBirthday(){
+        this.age += 1;
+    }
+
+    showAge(){
+        return this.age;
+    }
 }
